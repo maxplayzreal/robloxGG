d1ce881efbb5192771f70b1f9f9c358126b17bb3
MAXPLAYZ <126032950+maxplayzreal@users.noreply.github.com>
2025-08-24 00:20:56 +0000

    a

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (cluster.isMaster) {
 } else {
   const app = express();
 
-  const nggurl = "https://www.youtubeunblocked.live";
+  const nggurl = "https://www.autistici.org/burnedprojects/UNDERTALE";
 
   const proxy = httpProxy.createProxyServer({
     target: nggurl,
