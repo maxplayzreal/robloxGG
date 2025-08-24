08d0aae4f759632be5f6b471945f108334cc824c
MAXPLAYZ <126032950+maxplayzreal@users.noreply.github.com>
2025-08-24 00:07:07 +0000

    a

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if (cluster.isMaster) {
 } else {
   const app = express();
 
-  const nggurl = "https://open.spotify.com";
+  const nggurl = "https://accounts.spotify.com/en/login";
 
   const proxy = httpProxy.createProxyServer({
     target: nggurl,
